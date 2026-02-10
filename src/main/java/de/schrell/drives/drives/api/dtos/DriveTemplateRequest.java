ada59ca1a5f4362a56b90aab0c69f4184b6388ca
Andreas Schrell <as@schrell.de>
2026-02-10 18:22:34 +0100

    Von und Nach dürfen ein Leerzeichen sein

--- src/main/java/de/schrell/drives/drives/api/dtos/DriveTemplateRequest.java
+++ src/main/java/de/schrell/drives/drives/api/dtos/DriveTemplateRequest.java
@@ -3,14 +3,15 @@ package de.schrell.drives.drives.api.dtos;
 import de.schrell.drives.drives.domain.entities.Reason;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record DriveTemplateRequest(
         String id,
         @NotBlank(message = "Name is required") String name,
         @Min(value = 0, message = "Drive length must be at least 0") int driveLength,
-        @NotBlank(message = "From location is required") String fromLocation,
-        @NotBlank(message = "To location is required") String toLocation,
+        @NotEmpty(message = "From location is required") String fromLocation,
+        @NotEmpty(message = "To location is required") String toLocation,
         @NotNull(message = "Reason is required") Reason reason
 ) {
 }
