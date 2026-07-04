4bbf7d0f9325796ed9c82587ee8734771536c25c
Andreas Schrell <as@schrell.de>
2026-07-04 08:46:53 +0200

    chore: remove empty startup runner

--- src/main/java/de/schrell/drives/FahrtenApplication.java
+++ src/main/java/de/schrell/drives/FahrtenApplication.java
@@ -1,17 +1,9 @@
 package de.schrell.drives;
 
-import de.schrell.drives.drives.domain.repositories.DriveRepository;
-import de.schrell.drives.drives.domain.repositories.DriveTemplateRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -21,10 +13,4 @@ public class FahrtenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FahrtenApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner init(DriveTemplateRepository driveTemplatRepository, DriveRepository driveRepository, JdbcTemplate jdbcTemplate) {
-		return args -> {
-        };
-    }
 }
